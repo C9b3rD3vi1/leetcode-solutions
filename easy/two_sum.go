d241a6314cd251ae6513568e3c49efad412b1884
C9b3rD3vi1 <nicksonwe@gmail.com>
2025-07-28 17:07:50 +0300

    leetcode easy

--- easy/two_sum.go
+++ easy/two_sum.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 )
 
-// Calculate the two sum of a given array of integers and a target value.
+/*	
+ * 1. Two Sum
+ 
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ You can return the answer in any order.
+  
+ Example 1:
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+ Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
+
+ // Calculate the two sum of a given array of integers and a target value.
+
 func twoSum(nums []int, target int) []int {
 	seen := make(map[int]int)  // Initialize a map to store seen numbers and their indices
 	for i, num := range nums {
